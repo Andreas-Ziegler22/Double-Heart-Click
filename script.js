d2a8f2523ca1e8112410c448194a4ba632e9be5c
MrBurp <andreass.ziegler@gmail.com>
2022-10-29 12:34:51 +0200

    view point click

--- script.js
+++ script.js
@@ -8,9 +8,20 @@ loveMe.addEventListener("click", (e) => {
     clickTime = new Date().getTime();
   } else {
     if (new Date().getTime() - clickTime < 800) {
-      console.log(123);
+      createHeart(e);
+      clickTime = 0;
     } else {
       clickTime = new Date().getTime();
     }
   }
 });
+
+const createHeart = (e) => {
+  const heart = document.createElement("i");
+  heart.classList.add("fas");
+  heart.classList.add("fa-heart");
+
+  const x = e.clientX;
+  const y = e.clientY;
+  console.log(x, y);
+};
