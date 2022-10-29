0a9f5910ebbca0d4d9bcdba1228d5e8f27b2d887
MrBurp <andreass.ziegler@gmail.com>
2022-10-29 12:39:31 +0200

    inside de img  x and y

--- script.js
+++ script.js
@@ -23,5 +23,12 @@ const createHeart = (e) => {
 
   const x = e.clientX;
   const y = e.clientY;
-  console.log(x, y);
+
+  const leftOffset = e.target.offsetLeft;
+  const topOffset = e.target.offsetTop;
+
+  const xInside = x - leftOffset;
+  const yInside = y - topOffset;
+
+  console.log(xInside, yInside);
 };
